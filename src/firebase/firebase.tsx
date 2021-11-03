2dabd571472f5ae3f8426f171c1d4d14c5046785
federicoWax <33701629+federicoWax@users.noreply.github.com>
2021-11-02 17:23:30 -0700

    Update firebase.tsx

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -11,14 +11,7 @@ export interface RootState {
 }
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBjUjG3QFu1LtEGaakJTq6FN6Jrm2lJ-rg",
-  authDomain: "ezjobs-60240.firebaseapp.com",
-  databaseURL: "https://ezjobs-60240-default-rtdb.firebaseio.com",
-  projectId: "ezjobs-60240",
-  storageBucket: "ezjobs-60240.appspot.com",
-  messagingSenderId: "1042492707043",
-  appId: "1:1042492707043:web:a01d3a9d25d21772fbf5d6",
-  measurementId: "G-T6RV9PSXKF"
+ 
 });
 
 firebase.firestore();
